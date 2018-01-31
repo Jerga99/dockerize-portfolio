001a93682a11a80969fadc67ea0c5bb5fde77fdd
Filip Jerga <filipjerga@Filips-MacBook-Pro.local>
2018-01-31 22:29:15 +0100

    Add concer into application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,5 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include DeviseWhitelist
   include SetSource
-
-  def current_user
-    super || OpenStruct.new(name: 'Guest', first_name: 'Guest', last_name: 'Guest', email: "no email")
-  end
+  include CurrentUserConcern
 end
