dd570499b3cc65065d0756e5549b3c4fcaa58b4c
Filip Jerga <filipjerga@Filips-MacBook-Pro.local>
2018-02-04 14:30:57 +0100

    Add styles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,12 @@
 module ApplicationHelper
 
-  def login_helper
+  def login_helper style = ''
     if current_user.is_a?(GuestUser)
-      (link_to "Register", new_user_registration_path) +
-      "<br>".html_safe +
-      (link_to "Login", new_user_session_path)
+      (link_to "Register", new_user_registration_path, class: style) +
+      " ".html_safe +
+      (link_to "Login", new_user_session_path, class: style)
     else
-      link_to("Logout", destroy_user_session_path, method: :delete)
+      link_to("Logout", destroy_user_session_path, class: style, method: :delete)
     end
   end
 
@@ -20,4 +20,40 @@ module ApplicationHelper
   def copyright_generator
     @copyright = JergaViewTool::Renderer.copyright "Filip Jerga", "All rights reserved"
   end
+
+  def nav_items
+    [
+      {
+        url: root_path, title: "Home"
+      },
+      {
+        url: about_me_path, title: "About me"
+      },
+      {
+        url: contact_path, title: "Contact"
+      },
+      {
+        url: blogs_path, title: "Blog"
+      },
+      {
+        url: portfolios_path, title: "Portfolios"
+      }
+    ]
+  end
+
+  def nav_helper style, tag_type
+    nav_links = ''
+
+    nav_items.each do |link|
+      nav_links << "<#{tag_type}><a href='#{link[:url]}' class='#{style} #{active? link[:url]}'> #{link[:title]} </a></#{tag_type}>"
+    end
+
+    nav_links.html_safe
+  end
+
+  def active? path
+    "active" if current_page? path
+  end
+
+
 end
