030ed710b9807f9b056003f9ab8bfbd0ab460d4f
Filip Jerga <filipjerga@Filips-MacBook-Pro.local>
2018-02-08 14:42:14 +0100

    Set assets to public

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,6 @@ module MyPortfolio
     config.generators.system_tests = nil
     config.eager_load_paths << "#{Rails.root}/lib"
     config.secret_key_base = ENV["SECRET_KEY_BASE"]
+    config.serve_static_assets = true
   end
 end
